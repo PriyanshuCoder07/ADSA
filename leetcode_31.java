85847b5af295acca528710992177c28ef1f2a8eb
PriyanshuCoder07 <kumarpriyanshu954@gmail.com>
2025-09-27 01:07:31 +0530

    question no 31 modified

--- leetcode_31.java
+++ leetcode_31.java
@@ -1,1 +1,7 @@
-System.out.println("hello");
+public class leetcode_31{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("yes it is for testing");
+    }
+
+}
